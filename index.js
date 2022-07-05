44daf56e70f6383195c305831519baebdd0dd573
LucaYoy <lucaion781@gmail.com>
2022-07-05 14:20:55 +0300

    Leed tracker app, chrome extension. Did server for it and used Nedb for database storage

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Datastore = require('nedb')
+const db = new Datastore('database.db')
+const PORT = 3000
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cors())
+db.loadDatabase()
+
+//listen for GET requests and send leads from Db
+app.get('/api/start', (req,res) => {
+	console.log('I got a GET request')
+	db.find({}).sort({_id:1}).exec( (err,leedsArr) => {
+		if (leedsArr.length !== 0) {
+			//console.log(leedsArr)
+			res.json(JSON.stringify(leedsArr[leedsArr.length-1]))
+		} else {
+			res.end('null')
+		}
+	})
+})
+
+app.get('/api/clear', (req,res) => {
+	console.log('I got a GET request')
+	db.remove({}, {multi: true}, (err) => console.error(err))
+	res.end()
+})
+
+//Listen for POST requests and save leeds to Db
+app.post('/api/update', (req,res) => {
+	console.log('I got a POST request!')
+	//console.log(req.body)
+	const data = req.body
+	db.insert(data, (err, data) => console.log(data))
+	res.end()
+})
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
---
+++ public/app.js
@@ -0,0 +1,82 @@
+let myLeads = []
+let count = 0
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+const copyBtn = document.getElementById("copy-btn")
+let options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}
+
+//Get Myleads from Db
+fetch('http://localhost:3000/api/start').then( res => res.json()).then( leeds => {
+    //console.log(leeds)
+    if (leeds) {
+        myLeads = JSON.parse(leeds).myLeads
+        count = myLeads.length
+        render(myLeads)
+    }
+}).catch(err => console.error(err)) 
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+function sendToServer() {
+    const leedsObj = {myLeads: myLeads, _id: count}
+    options.body = JSON.stringify(leedsObj)
+    //console.log(options)
+    fetch('http://localhost:3000/api/update',options)
+}
+
+copyBtn.addEventListener("click", function() {
+    let clipboardTxt = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        clipboardTxt += myLeads[i]+"\n"
+    }
+    navigator.clipboard.writeText(clipboardTxt)
+})
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        count += 1
+        sendToServer() //Send leeds to server and save then in Db
+        render(myLeads)
+    })
+})
+
+
+
+deleteBtn.addEventListener("dblclick", function() {
+    //localStorage.clear()
+    fetch('http://localhost:3000/api/clear')
+    myLeads = []
+    count = 0
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value) {
+        myLeads.push(inputEl.value)
+        count += 1
+        inputEl.value = ""
+        sendToServer() //Send leeds to server and save then in Db
+        render(myLeads)
+    }
+})
